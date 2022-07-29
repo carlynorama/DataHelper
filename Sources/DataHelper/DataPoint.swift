b65d438162fdd39d9de3ff4f393728f05a29906b
carlynorama <carlynorama@gmail.com>
2022-07-29 10:34:34 -0700

    unzipping DataPoints

--- Sources/DataHelper/DataPoint.swift
+++ Sources/DataHelper/DataPoint.swift
@@ -16,5 +16,29 @@ public protocol DataPoint {
 }
 
 
+extension Array {
+
+    /// Unzip an `Array` of key/value tuples.
+    ///
+    /// - Returns: A tuple with two arrays, an `Array` of keys and an `Array` of values.
+
+    func unzip<Number, Number>() -> ([Number], [Number]) where Element == DataPoint {
+        var inputs = [Number]()
+        var results = [Number]()
+
+        inputs.reserveCapacity(count)
+        results.reserveCapacity(count)
+
+        forEach { x, y in
+            inputs.append(x)
+            results.append(y)
+        }
+
+        return (inputs, results)
+    }
+}
+
+
+
 
 
--- Sources/DataHelper/Extensions/Array+Unzip.swift
+++ Sources/DataHelper/Extensions/Array+Unzip.swift
@@ -8,25 +8,25 @@
 import Foundation
 
 
-//https://stackoverflow.com/questions/46043902/opposite-of-swift-zip-split-tuple-into-two-arrays
-extension Array {
-    
-    /// Unzip an `Array` of key/value tuples.
-    ///
-    /// - Returns: A tuple with two arrays, an `Array` of keys and an `Array` of values.
-
-    func unzip<K, V>() -> ([K], [V]) where Element == (key: K, value: V) {
-        var keys = [K]()
-        var values = [V]()
-
-        keys.reserveCapacity(count)
-        values.reserveCapacity(count)
-
-        forEach { key, value in
-            keys.append(key)
-            values.append(value)
-        }
-
-        return (keys, values)
-    }
-}
+////https://stackoverflow.com/questions/46043902/opposite-of-swift-zip-split-tuple-into-two-arrays
+//extension Array {
+//    
+//    /// Unzip an `Array` of key/value tuples.
+//    ///
+//    /// - Returns: A tuple with two arrays, an `Array` of keys and an `Array` of values.
+//
+//    func unzip<K, V>() -> ([K], [V]) where Element == (key: K, value: V) {
+//        var keys = [K]()
+//        var values = [V]()
+//
+//        keys.reserveCapacity(count)
+//        values.reserveCapacity(count)
+//
+//        forEach { key, value in
+//            keys.append(key)
+//            values.append(value)
+//        }
+//
+//        return (keys, values)
+//    }
+//}
