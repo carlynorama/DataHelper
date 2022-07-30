a544f0c9a5f2449f128e28740ac662ef14fc3f3b
carlynorama <carlynorama@gmail.com>
2022-07-30 04:57:18 -0700

    move meat of findline function to solvers

--- Sources/DataHelper/DataSet.swift
+++ Sources/DataHelper/DataSet.swift
@@ -56,22 +56,11 @@ public extension Array where Element == DataPoint {
     }
     
     func findLine() -> (m:Number, b:Number) {
-        let splitData = self.unzipDataPoints()
-        
-        let cx1_SigmaXk2:Number = splitData.inputs.sumOfSquares()
-        let cy1_SigmaXk:Number = splitData.inputs.reduce(0, +)
-        let cx2_SigmaXk:Number = splitData.inputs.reduce(0, +)
-        let cy2_n:Number = Double(self.count) as Number
-        
-        let xkyk = vDSP.multiply(splitData.inputs, splitData.outputs)
-        
-        let s1_SigmaXkYk:Number = xkyk.reduce(0, +)
-        let s2_SigmaYk:Number = splitData.outputs.reduce(0, +)
-        
-        let result = DataHelper.solveLinearPair(cx1: cx1_SigmaXk2, cy1: cy1_SigmaXk, s1: s1_SigmaXkYk, cx2: cx2_SigmaXk, cy2: cy2_n, s2: s2_SigmaYk)
-        
+        let result = DataHelper.findLine(for: self)
         return (result.x, result.y)
     }
+    
+    
     
 }
 
--- Sources/DataHelper/Solvers.swift
+++ Sources/DataHelper/Solvers.swift
@@ -9,11 +9,28 @@ import Foundation
 import Accelerate
 import simd
 
-
-
 //https://developer.apple.com/documentation/accelerate/solving_systems_of_linear_equations_with_lapack
 //https://developer.apple.com/documentation/accelerate/using_vdsp_for_vector-based_arithmetic
+
+
 public extension DataHelper {
+    
+    static func findLine(for data:[DataPoint]) -> SIMD2<Number> {
+        let splitData = data.unzipDataPoints()
+        let cx1_SigmaXk2:Number = splitData.inputs.sumOfSquares()
+        let cy1_SigmaXk:Number = splitData.inputs.reduce(0, +)
+        let cx2_SigmaXk:Number = splitData.inputs.reduce(0, +)
+        let cy2_n:Number = Double(data.count) as Number
+        
+        let xkyk = vDSP.multiply(splitData.inputs, splitData.outputs)
+        
+        let s1_SigmaXkYk:Number = xkyk.reduce(0, +)
+        let s2_SigmaYk:Number = splitData.outputs.reduce(0, +)
+        
+        let result = DataHelper.solveLinearPair(cx1: cx1_SigmaXk2, cy1: cy1_SigmaXk, s1: s1_SigmaXkYk, cx2: cx2_SigmaXk, cy2: cy2_n, s2: s2_SigmaYk)
+        
+        return result
+    }
     
     
     //You can use matrices to solve simultaneous equations of the form AX = B
